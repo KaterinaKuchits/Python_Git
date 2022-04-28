447f2c5438f795b520dff42c82d3123e38362144
KaterinaKuchits <ekuchits@gmail.com>
2022-04-28 17:26:14 +0300

    lesson_2

---
+++ lesson_2_dz_1.py
@@ -0,0 +1,21 @@
+# 1. Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+# print(d, type(d))
+
+# Решение
+# 45 <class 'int'>
+# 5.0 <class 'float'>
+# 7 <class 'int'>
+# 225 <class 'int'>
